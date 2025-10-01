1465e3fa71e7d99d4898f874a44805f12c60c8fd
Marta Matijević <marta.matijevic@gmail.com>
2025-10-01 10:52:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('JS loaded');
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('nav ul');
+  if(burger){
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  }
+});
